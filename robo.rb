4e0434927b6220af287b5ee266a70b251bb32c48
agush <agush@roca9.com>
2016-03-09 17:49:27 -0600

    Does not allow to create a rover with invalid arguments

--- robo.rb
+++ robo.rb
@@ -2,6 +2,9 @@ class Rover
   @@positions = %w(N E S W)
   attr_reader :x, :y, :p
   def initialize(x=1,y=1,p='N')
+    if x < 0 || y < 0 || !@@positions.include?(p)
+      raise ArgumentError.new("Not a valid argument")
+    end
     @x = x
     @y = y
     @p = p
--- test_robo.rb
+++ test_robo.rb
@@ -10,4 +10,15 @@ describe Rover do
     rover.y.must_equal 1
     rover.p.must_equal 'N'
   end
+  it "can be created with arguments" do
+    rover = Rover.new(3,4,'E')
+    rover.must_be_instance_of Rover
+    rover.x.must_equal 3
+    rover.y.must_equal 4
+    rover.p.must_equal 'E'
+  end
+
+  it "can not be created with wrong arguments" do
+   lambda {Rover.new(3,4,'Y')}.must_raise(ArgumentError)
+  end
 end
