11c57b9f1ee999d48777d86d9873bca76443ec61
agush <agush@roca9.com>
2016-03-09 18:41:27 -0600

    Rover can rotate right and left

--- robo.rb
+++ robo.rb
@@ -1,12 +1,26 @@
 class Rover
-  @@positions = %w(N E S W)
+  @@pos = %w(N E S W)
   attr_reader :x, :y, :p
   def initialize(x=1,y=1,p='N')
-    if x < 0 || y < 0 || !@@positions.include?(p)
+    if x < 0 || y < 0 || !@@pos.include?(p)
       raise ArgumentError.new("Not a valid argument")
     end
     @x = x
     @y = y
     @p = p
+  end
+
+  def instruction(str)
+    str.chars.each do |ins|
+      if ins == 'M'
+        move
+      elsif ins == 'L'
+        @p = @@pos[(@@pos.index(@p) + @@pos.count - 1)% @@pos.count]
+      elsif ins == 'R'
+        @p = @@pos[(@@pos.index(@p) + 1)% @@pos.count]
+      else
+        raise "Unknown instruction"
+      end
+    end
   end
 end
